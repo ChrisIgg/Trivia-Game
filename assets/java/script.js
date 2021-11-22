07c37771d0257a5893d5ebea5f1d9c4e1a799349
Chris Insignares <insignaresc115@gmail.com>
2021-11-21 21:14:42 -0500

    update

---
+++ assets/java/script.js
@@ -0,0 +1,32 @@
+const startGame = document.querySelector("#startGameButton")
+
+const answerDisplay = document.createElement('i')
+answerDisplay.addEventListener('click', generatelist);
+
+generateList() {
+    
+}
+
+
+localStorage
+// activity 22//
+
+
+// storing as an array is storing objects is one way to create question storage
+
+// 1 question, four answers, one correct answer
+
+// form, creates a submit event
+
+// Crud, create, read, update, display.  Make a function for it
+
+// span to add items in succession (activity 24 has example in 04) element.matches/todo-list/
+
+// but
+
+// document.addEventListener('keydown', function(event){
+//     if(event.keycode === right)
+//     return win
+
+// function start cloc
+    
